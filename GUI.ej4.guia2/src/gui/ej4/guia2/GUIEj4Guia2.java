e3bc1f67507cbf6b7c1b6941550b3888028502db
Lourrios <141644661+Lourrios@users.noreply.github.com>
2023-08-22 10:12:52 -0300

    Add files via upload

---
+++ GUI.ej4.guia2/src/gui/ej4/guia2/GUIEj4Guia2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui.ej4.guia2;
+
+/**
+ *
+ * @author Lourdes
+ */
+public class GUIEj4Guia2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
